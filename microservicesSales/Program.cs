da168fa666fd3f945c3e96a4aa08363a68c4463a
Darwin <Darwin@DESKTOP-7K0L94R>
2026-01-21 01:47:31 -0600

    configuraciones de docker, componse y generales

--- microservicesSales/Program.cs
+++ microservicesSales/Program.cs
@@ -1,3 +1,10 @@
+using microservicesSales.Application.Interfaces;
+using microservicesSales.Application.UseCase;
+using microservicesSales.Infrastructure.Hubs;
+using microservicesSales.Infrastructure.Repository;
+using microservicesSales.Infrastructure.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,11 +14,41 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("SalesDb")));
+
+
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ISaleRepository, SalesRepository>();
+builder.Services.AddScoped<IUnitOfWork, EfUnitOfWork>();
+builder.Services.AddScoped<ProductUseCase>();
+builder.Services.AddScoped<SalesUseCase>();
+
+
+builder.Services.AddSignalR();
+builder.Services.AddScoped<ISalesNotifier, SalesNotifier>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("SignalRCors", policy =>
+    {
+        policy
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .SetIsOriginAllowed(_ => true); 
+    });
+});
+
 var app = builder.Build();
 
+app.UseCors("SignalRCors");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+
+   
     app.UseSwagger();
     app.UseSwaggerUI();
 }
@@ -21,5 +58,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<SalesHub>("/hubs/sales");
 
 app.Run();
---
+++ microservicesSales/db/migrate.sql
@@ -0,0 +1,82 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260121000151_Inicial'
+)
+BEGIN
+    CREATE TABLE [PRODUCTS] (
+        [Id] uniqueidentifier NOT NULL,
+        [Name] nvarchar(100) NOT NULL,
+        [Price] decimal(18,2) NOT NULL,
+        [Stock] int NOT NULL,
+        [RowVersion] rowversion NOT NULL,
+        CONSTRAINT [PK_PRODUCTS] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260121000151_Inicial'
+)
+BEGIN
+    CREATE TABLE [SALES] (
+        [Id] uniqueidentifier NOT NULL,
+        [Date] datetime2 NOT NULL,
+        CONSTRAINT [PK_SALES] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260121000151_Inicial'
+)
+BEGIN
+    CREATE TABLE [SALEITEMS] (
+        [Id] uniqueidentifier NOT NULL,
+        [SaleId] uniqueidentifier NOT NULL,
+        [ProductId] uniqueidentifier NOT NULL,
+        [ProductName] nvarchar(100) NOT NULL,
+        [Quantity] int NOT NULL,
+        [UnitPrice] decimal(18,2) NOT NULL,
+        CONSTRAINT [PK_SALEITEMS] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_SALEITEMS_SALES_SaleId] FOREIGN KEY ([SaleId]) REFERENCES [SALES] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260121000151_Inicial'
+)
+BEGIN
+    CREATE INDEX [IX_SALEITEMS_SaleId] ON [SALEITEMS] ([SaleId]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260121000151_Inicial'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20260121000151_Inicial', N'8.0.23');
+END;
+GO
+
+COMMIT;
+GO
+
